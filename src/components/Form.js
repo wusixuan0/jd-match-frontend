23b841befb16b1271dc0eb43c405a875a0d663ae
Sixuan Wu <wusixuan2@icloud.com>
2024-06-18 16:31:24 -0400

    add form submission logic

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,31 +7,51 @@ import Markdown from 'react-markdown'
 const Form = () => {
     const [formData, setFormData] = useState({});
     const [responseText, setResponseText] = useState('');
+    const [warning, setWarning] = useState('');
+
     const handleInputChange = (e) => {
         setFormData({
             ...formData,
             [e.target.name]: e.target.value,
         });
+        setWarning('');
     };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+        
         let promptText = prompt['prompt'];
-
-        if (Object.keys(formData).length === 0) {
+        const apiUrl = 'http://localhost:3001/api/';
+        
+        if (Object.keys(formData).length == 0) {
+        
             inputFields.forEach((question) => {
                 promptText += `\n${question['title']}: ${question['sample_response']}`;
             });
-        }
-        promptText += prompt['add_prompt']
-        const apiUrl = 'http://localhost:3001/api/';
-        const testDisplayUrl = 'http://localhost:3001/api/';
 
-        try {
-            // const response = await axios.post(apiUrl, { prompt: promptText});
-            const response = await axios.get(testDisplayUrl);
-            setResponseText(response.data.responseText);
-        } catch (error) {
-            console.error(error);
+            promptText += prompt['add_prompt']
+
+            try {
+                const response = await axios.get(apiUrl);
+                setResponseText(response.data.responseText);
+            } catch (error) {
+                console.error(error);
+            }
+        } else if (Object.keys(formData).length < inputFields.length) {
+            setWarning('Please fill out all fields.');
+        } else {
+            inputFields.forEach((question) => {
+                const userAnswer = formData[`${question.id}-input`] || '';
+                promptText += `\n${question['title']}: ${userAnswer}`;
+            });
+            promptText += prompt['add_prompt']
+            
+            try {
+                const response = await axios.post(apiUrl, { prompt: promptText});
+                setResponseText(response.data.responseText);
+            } catch (error) {
+                console.error(error);
+            }
         }
         
     }        
@@ -39,6 +59,12 @@ const Form = () => {
         <div className="app">
             <h1>Job Description Builder</h1>
             <p>Generate a job description with <a href="https://gemini.google.com">Google Gemini</a> in few seconds.</p>
+            <div><a href="https://github.com/wusixuan0/recruit_stream/blob/main/README.md">description</a> <a href="https://github.com/wusixuan0/recruit_stream">front end</a> <a href="https://github.com/wusixuan0/recruit_express_server">back end</a></div>
+            <ul>
+                <li><strong>Handle No Input:</strong> If you haven't filled in any fields, it will submit the sample response displayed in the text box to generate a job description at the bottom.</li>
+                <li><strong>Incomplete Fields:</strong> If you've filled out some but not all of the 11 required fields, it shows a message to fill all fields.</li>
+                <li><strong>Complete Submission:</strong> Once all fields are filled, it will submit your responses to Google Gemini API to generate a job description.</li>
+            </ul>
             <form onSubmit={handleSubmit}>
             {inputFields.map((item) => (
                 
@@ -60,8 +86,10 @@ const Form = () => {
                 
             ))}
             <button type="submit">Create Job Description</button>
+            {warning && <p className="warning">{warning}</p>}
             </form>
-            <Markdown className="jd">{responseText}</Markdown>
+            
+            {responseText && <Markdown className="jd">{responseText}</Markdown>}
         </div>
     );
 };
