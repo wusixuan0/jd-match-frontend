53c0eb91addee8c096f23b86c46d0300d8f77b35
Sixuan Wu <wusixuan2@icloud.com>
2024-08-09 11:25:00 -0400

    add select

--- src/components/UploadPDF.js
+++ src/components/UploadPDF.js
@@ -3,16 +3,17 @@ import axios from 'axios';
 import Markdown from 'react-markdown'
 
 const PDFUploadForm = () => {
-  const [pdfFile, setPdfFile] = useState(null);
-  const [responseList, setResponseList] = useState('');
-  const [error, setError] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+    const [pdfFile, setPdfFile] = useState(null);
+    const [version, setVersion] = useState('version1');
+    const [responseList, setResponseList] = useState('');
+    const [error, setError] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
-  const handleFileChange = (event) => {
-    setPdfFile(event.target.files[0]);
-  };
+    const handleFileChange = (event) => {
+        setPdfFile(event.target.files[0]);
+    };
 
-  const handleSubmit = async (event) => {
+    const handleSubmit = async (event) => {
     event.preventDefault();
 
     if (!pdfFile) return;
@@ -24,71 +25,80 @@ const PDFUploadForm = () => {
     const API_BASE_URL = process.env.REACT_APP_API_URL;
 
     if (API_BASE_URL) {
-      console.log("API URL found", API_BASE_URL)
+        console.log("API URL found", API_BASE_URL)
     } else {
-      console.log("API URL not found")
+        console.log("API URL not found")
     }
 
     try {
-      const formData = new FormData();
-      formData.append('file', pdfFile);
-      const response = await axios.post(API_BASE_URL, formData, {
+        const formData = new FormData();
+        formData.append('file', pdfFile);
+        formData.append('version', version);
+        const response = await axios.post(API_BASE_URL, formData, {
         headers: {
-          'Content-Type': 'multipart/form-data',
+            'Content-Type': 'multipart/form-data',
         },
-      });
-      
-      const ranked_jds=response?.data?.ranked_jds
-      setResponseList(ranked_jds);
-      setIsLoading(false);
+    });
+
+        const ranked_jds=response?.data?.ranked_jds
+        setResponseList(ranked_jds);
+        setIsLoading(false);
     } catch (error) {
-      setError(error?.response?.data?.error);
+        setError(error?.response?.data?.error);
     }
-  };
+    };
 
-  const NestedJSON = ({ data }) => {
-    const filteredData = { ...data };
-    delete filteredData?.description;
+    const NestedJSON = ({ data }) => {
+        const filteredData = { ...data };
+        delete filteredData?.description;
+        return (<pre>{JSON.stringify(data, null, 2)}</pre>);
+    };
+
     return (
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    );
-  };
+        <div className="upload-page">
+            
+            <h2>AI-Powered Job Match Solution</h2>
+            <b>Project Overview: </b>
+            <p>This solution leverages Large Language Models (LLMs) to streamline the job search process. It takes a user's resume in PDF format as input and analyzes it to extract key skills, experiences, and career goals. The extracted information is then compared against a vast dataset of 51,863 job listings stored in OpenSearch, which have been collected from Google Jobs.  The system then identifies and returns the top 5 job descriptions that are the best match for the user's profile.</p>
+            <a href="https://carbonated-waxflower-92e.notion.site/b978cea7fa9a4f2ab72558e9ff101ddf?pvs=4">Demo, Detailed Explanation, and Future Release</a>
+            <div> 
+                View My Code: <a href="https://github.com/wusixuan0/jd-match-api">Backend</a> <a href="https://github.com/wusixuan0/jd-match-frontend">Frontend</a>  
+            </div>
+            <p>Instruction: Upload resume PDF to get top 5 job posts.</p>
+            <div>Version 1: Google Gemini to assess, match, and rank job descriptions</div>
+            <p>Version 2: Combines semantic search with embeddings and Gemini. More versions to come. See link for future release. </p>
+            <form onSubmit={handleSubmit}>
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <h2>AI-Powered Job Match Solution</h2>
-        <b>Project Overview: </b>
-        <p>This solution leverages Large Language Models (LLMs) to streamline the job search process. It takes a user's resume in PDF format as input and analyzes it to extract key skills, experiences, and career goals. The extracted information is then compared against a vast dataset of 51,863 job listings stored in OpenSearch, which have been collected from Google Jobs.  The system then identifies and returns the top 5 job descriptions that are the best match for the user's profile.</p>
-        <div> 
-          <a href="https://github.com/wusixuan0/jd-match-api">back end</a> <a href="https://github.com/wusixuan0/jd-match-frontend">front end</a> <a href="https://carbonated-waxflower-92e.notion.site/b978cea7fa9a4f2ab72558e9ff101ddf?pvs=4">notion</a> 
-          
+                <select value={version} 
+                    onChange={(e) => setVersion(e.target.value)}
+                    className="version-select"
+                >
+                    <option value="version1">Version 1</option>
+                    <option value="version2">Version 2</option>
+                </select>
+                <input
+                    type="file"
+                    accept="application/pdf"
+                    onChange={handleFileChange}
+                />
+                <button type="submit"
+                    className="upload-button"
+                    disabled={isLoading || !pdfFile}
+                >
+                    {isLoading ? 'Uploading...' : 'Upload'}
+                </button>
+            </form>
+            {error}
+            <ul>
+                { Object.values(responseList).map((jd) => (
+                    <div key={jd?._id}>
+                        <Markdown>{jd?._source?.description}</Markdown>
+                        <NestedJSON data={jd?._source} />
+                    </div>
+                )) }
+            </ul>
         </div>
-        <p>Instruction: Upload resume PDF to get top 5 job posts</p>
-        <input
-          type="file"
-          accept="application/pdf"
-          onChange={handleFileChange}
-        />
-        <button type="submit"
-         className="upload-button"
-         disabled={isLoading || !pdfFile}
-        >
-          {isLoading ? 'Uploading...' : 'Upload'}
-        </button>
-      </div>
-      {error}
-      <ul>
-      {Object.values(responseList).map((jd) => (
-        
-        <div key={jd?._id}>
-          <Markdown>{jd?._source?.description}</Markdown>
-          <NestedJSON data={jd?._source} />
-        </div>
-      ))}
-      </ul>
-    </form>
-  );
+    );
 };
 
 export default PDFUploadForm;
