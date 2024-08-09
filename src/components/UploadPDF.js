3b735d4e36ffa97e2bd0f35f4bacd0ee84b195c1
Sixuan Wu <wusixuan2@icloud.com>
2024-08-08 21:49:29 -0400

    update notion link

--- src/components/UploadPDF.js
+++ src/components/UploadPDF.js
@@ -57,11 +57,11 @@ const PDFUploadForm = () => {
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <h2>Resume Match</h2>
+        <h2>AI-Powered Job Match Solution</h2>
         <b>Project Overview: </b>
-        <p>A end-to-end LLM solution for job match that takes a user's resume PDF as input and returns the top 5 best-matching job descriptions from a dataset of 51,863 job listings stored in OpenSearch, scraped from Google Jobs.</p>
+        <p>This solution leverages Large Language Models (LLMs) to streamline the job search process. It takes a user's resume in PDF format as input and analyzes it to extract key skills, experiences, and career goals. The extracted information is then compared against a vast dataset of 51,863 job listings stored in OpenSearch, which have been collected from Google Jobs.  The system then identifies and returns the top 5 job descriptions that are the best match for the user's profile.</p>
         <div> 
-          <a href="https://github.com/wusixuan0/jd-match-api">back end</a> <a href="https://github.com/wusixuan0/jd-match-frontend">front end</a> <a href="https://carbonated-waxflower-92e.notion.site/bf05026ee8264c0bb20395f4ccd3cd6e?pvs=4">notion</a> 
+          <a href="https://github.com/wusixuan0/jd-match-api">back end</a> <a href="https://github.com/wusixuan0/jd-match-frontend">front end</a> <a href="https://carbonated-waxflower-92e.notion.site/b978cea7fa9a4f2ab72558e9ff101ddf?pvs=4">notion</a> 
           
         </div>
         <p>Instruction: Upload resume PDF to get top 5 job posts</p>
